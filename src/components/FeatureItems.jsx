82105f6a10283f6fa6aef5b6ab12870031ce438a
Laurent Thorigny <laurent.thorigny@orange.fr>
2024-07-28 15:39:16 +0200

    p11-correction lien absolu & relatif

--- src/components/FeatureItems.jsx
+++ src/components/FeatureItems.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import data from '../assets/json/featureItems.json'
 
 
-
 const FeatureItens = () => {
   return (
     <div className="features">
         {data.map(item => (
         <div key={item.id} className="feature-item"> {/* Utilisez un attribut key unique, ici item.id */}
-
           <img src={item.img}  alt="Chat Icon" className="feature-icon" />   
           <h3>{item.title}</h3>
           <p>{item.description}</p>
-          <p>{item.img}</p>
-         
         </div>
         
       ))}
@@ -21,5 +17,5 @@ const FeatureItens = () => {
     </div>
   );
 };
-
+console.log(data)
 export default FeatureItens;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,3 @@
-import featureImg1 from '../assets/img/icon-chat.png'
-import featureImg2 from '../assets/img/icon-money.png'
-import featureImg3 from '../assets/img/icon-security.png'
 import FeatureItens from '../components/FeatureItems'
 
 
@@ -19,42 +16,7 @@ const Home = () => {
             </div>
             <section className="features">
                 <h2 className="sr-only">Features</h2>
-                {/*
-                <div className="feature-item">
-                    <img src={featureImg1} alt="Chat Icon" className="feature-icon" />
-                    <h3 className="feature-item-title">You are our #1 priority</h3>
-                    <p>
-                        Need to talk to a representative? You can get in touch through our
-                        24/7 chat or through a phone call in less than 5 minutes.
-                    </p>
-                </div>
-                <div className="feature-item">
-                    <img
-                        src={featureImg2}
-                        alt="Chat Icon"
-                        className="feature-icon"
-                    />
-                    <h3 className="feature-item-title">More savings means higher rates</h3>
-                    <p>
-                        The more you save with us, the higher your interest rate will be!
-                    </p>
-                </div>
-                <div className="feature-item">
-                    <img
-                        src={featureImg3}
-                        alt="Chat Icon"
-                        className="feature-icon"
-                    />
-                    <h3 className="feature-item-title">Security you can trust</h3>
-                    <p>
-                        We use top of the line encryption to make sure your data and money
-                        is always safe.
-                    </p>
-                </div>*/}
                 <FeatureItens />
-
-
-
             </section>
         </main>
     )
